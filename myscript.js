53d6e72e722affc4e1c8ea09eb131e2977fab529
iamtemilolah <107153328+iamtemilolah@users.noreply.github.com>
2022-06-24 13:27:30 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
-// console.log('Suara Temilola Barakat')
-// console.log('Height: 165cm');
-// console.log('Country: Nigeria');
+console.log('Suara Temilola Barakat')
+console.log('Height: 165cm');
+console.log('Country: Nigeria');
 
 
